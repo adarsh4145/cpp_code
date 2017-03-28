e9e42d25ab289f2356e5848e4faae6e300c763f1
Adarsh Kumar Pandey <adarshkumar4145@gmail.com>
2017-03-29 00:50:05 +0530

    Add files via upload

---
+++ bool.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(void){
+bool a;
+int x=1;
+int y=2;
+float z=5;
+short int q=5;
+long int w=2;
+double e=3;
+long double d=1;
+a=(x<y);
+cout<<"int: "<<x<<"\t\t size: "<<sizeof(x)<<endl;
+cout<<"long int: "<<w<<"\t\t size: "<<sizeof(w)<<endl;
+cout<<"flaot: "<<z<<"\t\t size: "<<sizeof(z)<<endl;
+cout<<"short int: "<<q<<"\t\t size: "<<sizeof(q)<<endl;
+cout<<"double: "<<e<<"\t\t size: "<<sizeof(e)<<endl;
+cout<<"long double: "<<d<<"\t\t size: "<<sizeof(d)<<endl;
+//cout<<"int: "<<x<<"\t\t size of int: "<<sizeof(x)<<endl;
+bool t=false;
+cout<<t<<endl;"hello \
+ 				deer "<<endl;
+cout<< 
+return 0;
+}
